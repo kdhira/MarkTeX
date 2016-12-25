ba7d133517103c1a8bca5c8dd3a875a737ec7091
Kevin Hira <khir664@aucklanduni.ac.nz>
2016-12-25 23:32:36 +1300

    Docstring fixes.

--- MarkTeX.py
+++ MarkTeX.py
@@ -83,13 +83,14 @@ class MarkTex:
         # Set default variables dictionary for LatexDocuments to base off.
         self.defaultvars = self.macros['defaults']
 
-        '''
+
+    def generateDocument(self, file):
+        """
         generateDocument() simply pieces the sub operations of compiling a document into one method.
         Some called could (maybe should) be chained or refactored into the LatexDocument itself.
 
         Process: Read front matter section, process front matter and write premable of document, parse content of document.
-        '''
-    def generateDocument(self, file):
+        """
         newDoc = LatexDocument(file, self)
 
         newDoc.readFrontMatter()
